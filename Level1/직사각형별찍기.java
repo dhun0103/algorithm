032661e836d717b9a8477c88928ba182739d3506
Jeongdonghun <arrrrr180@gmail.com>
2022-12-01 03:35:05 +0900

    feat : 직사각형별찍기

---
+++ Level1/직사각형별찍기.java
@@ -0,0 +1,22 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class 직사각형별찍기 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int y = 0; y < m; y++) {
+            for (int x = 0; x < n; x++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
