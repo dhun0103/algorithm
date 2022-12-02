090fa41c2a0f32e157ba97c82b6acc827ab1f217
Jeongdonghun <arrrrr180@gmail.com>
2022-12-03 04:08:30 +0900

    feat : 없는 숫자 더하기

--- Level1/문자열을_정수로_바꾸기.java
+++ Level1/문자열을_정수로_바꾸기.java
@@ -2,7 +2,7 @@ package Level1;
 
 public class 문자열을_정수로_바꾸기 {
 
-    public static int solution(String s){
+    public static int solution(String s) {
         return Integer.parseInt(s);
     }
 
---
+++ Level1/없는_숫자_더하기.java
@@ -0,0 +1,26 @@
+package Level1;
+
+public class 없는_숫자_더하기 {
+
+    public static int solution(int[] numbers) {
+
+        int[] check = new int[10];
+        int sum = 0;
+
+        for (int x = 0; x < numbers.length; x++) {
+            check[numbers[x]]++;
+        }
+
+        for (int x = 0; x < 10; x++) {
+            if (check[x] == 0) {
+                sum += x;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+}
