e86fc2e3a66c88042ac8c743c9d78089c8e37d6e
Jeongdonghun <arrrrr180@gmail.com>
2022-12-03 04:29:20 +0900

    feat : 핸드폰 번호 가리기

---
+++ Level1/핸드폰_번호_가리기.java
@@ -0,0 +1,20 @@
+package Level1;
+
+public class 핸드폰_번호_가리기 {
+
+    public static String solution(String phone_number) {
+
+        String str = "";
+        int len = phone_number.length();
+
+        for (int x = 0; x < len - 4; x++) {
+            str += "*";
+        }
+        return str+phone_number.substring(len-4);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("67457867"));
+    }
+}
