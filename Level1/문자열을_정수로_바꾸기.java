0d869fdc721a00bb72b9c063aaf03003e217e5bf
Jeongdonghun <arrrrr180@gmail.com>
2022-12-03 04:02:30 +0900

    feat : 문자열을 정수로 바꾸기

---
+++ Level1/문자열을_정수로_바꾸기.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class 문자열을_정수로_바꾸기 {
+
+    public static int solution(String s){
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("-1234"));
+    }
+}
