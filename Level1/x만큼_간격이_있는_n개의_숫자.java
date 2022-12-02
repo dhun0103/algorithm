a219e94f0f8b942d88d2bfa5fbc862494a4bf7fb
Jeongdonghun <arrrrr180@gmail.com>
2022-12-03 04:57:13 +0900

    feat : x만큼 간격이 있는 n개의 숫자

---
+++ Level1/x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,25 @@
+package Level1;
+
+public class x만큼_간격이_있는_n개의_숫자 {
+
+    public static long[] solution(long x, int n) {
+
+        long[] arr = new long[n];
+        long num = x;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = num;
+            num += x;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        long[] result = solution(-2, 5);
+
+        for (int x = 0; x < result.length; x++) {
+            System.out.println(result[x]);
+        }
+    }
+}
--- Level1/행렬의_덧셈.java
+++ Level1/행렬의_덧셈.java
@@ -16,5 +16,12 @@ public class 행렬의_덧셈 {
 
     public static void main(String[] args) {
 
+        int[][] result = solution(new int[][]{{1, 2}, {3, 4}}, new int[][]{{1, 2}, {3, 4}});
+
+        for (int y = 0; y < result.length; y++) {
+            for (int x = 0; x < result[0].length; x++) {
+                System.out.println(result[y][x]);
+            }
+        }
     }
 }
