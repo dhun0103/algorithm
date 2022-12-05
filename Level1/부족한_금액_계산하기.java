247e5a2e988dbf07d68a481623c5e7306693be0c
Jeongdonghun <arrrrr180@gmail.com>
2022-12-05 23:26:59 +0900

    feat : 부족한 금액 계산하기

---
+++ Level1/부족한_금액_계산하기.java
@@ -0,0 +1,26 @@
+package Level1;
+
+public class 부족한_금액_계산하기 {
+
+    public static long solution(int price, int money, int count) {
+
+        int sum = 0;
+        int plus = price;
+
+        for (long x = 0; x < count; x++) {
+            sum += plus;
+            plus += price;
+        }
+
+        if (sum <= money) {
+            return 0;
+        } else {
+            return sum - money;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(3, 20, 4));
+    }
+}
