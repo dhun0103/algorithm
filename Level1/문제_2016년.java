e3c2ee31eeb3308ea773eb613e6058cce7021f2d
Jeongdonghun <arrrrr180@gmail.com>
2022-12-09 23:49:56 +0900

    feat : 2016년

--- Level1/문제_2016년.java
+++ Level1/문제_2016년.java
@@ -2,5 +2,22 @@ package Level1;
 
 public class 문제_2016년 {
 
+    public static String solution(int a, int b) {
 
+        String[] str = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int[] arr = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int sum = 0;
+
+        for (int i = 0; i < a - 1; i++) {
+            sum += arr[i];
+        }
+        sum += b;
+
+        return str[sum % 7];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(5, 24));
+    }
 }
