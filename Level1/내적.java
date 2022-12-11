1d3f98421d609f8b31ae67f8eeb2ef2f01608443
Jeongdonghun <arrrrr180@gmail.com>
2022-12-11 23:26:36 +0900

    feat : 내적

---
+++ Level1/내적.java
@@ -0,0 +1,21 @@
+package Level1;
+
+public class 내적 {
+
+    public static int solution(int[] arr, int[] brr) {
+
+        int sum = 0;
+
+        for (int x = 0; x < arr.length; x++) {
+            sum += (arr[x] * brr[x]);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+        System.out.println(solution(a, b));
+    }
+}
