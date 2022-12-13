05a80e8a683053b88faf68f006d5bb90e4b5345b
Jeongdonghun <arrrrr180@gmail.com>
2022-12-14 00:00:02 +0900

    Feat : 문자열 내 p와 y의 개수

---
+++ Level1/문자열_내_p와_y의_개수.java
@@ -0,0 +1,24 @@
+package Level1;
+
+public class 문자열_내_p와_y의_개수 {
+
+    public static boolean solution(String s) {
+
+        String str = s.toUpperCase();
+        int cnt = 0;
+
+        for (int x = 0; x < str.length(); x++) {
+
+            if (str.charAt(x) == 'P') cnt++;
+            else if (str.charAt(x) == 'Y') cnt--;
+        }
+
+        if (cnt == 0) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("pyY"));
+    }
+}
