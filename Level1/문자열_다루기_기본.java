1cde4564a18e973c8bc286e552c73593e8ded676
Jeongdonghun <arrrrr180@gmail.com>
2022-12-21 18:55:15 +0900

    feat : 문자열 다루기 기본

---
+++ Level1/문자열_다루기_기본.java
@@ -0,0 +1,21 @@
+package Level1;
+
+public class 문자열_다루기_기본 {
+
+    public static boolean solution(String s) {
+
+        int len = s.length();
+
+        if (len != 4 && len != 6) return false;
+        else {
+            String temp = s.replaceAll("[^0-9]", "");
+
+            return temp.equals(s);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("a123"));
+    }
+}
