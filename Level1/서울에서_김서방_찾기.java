43883d211520dd326ec3b2fb967433682ed43bc6
Jeongdonghun <arrrrr180@gmail.com>
2022-12-21 19:10:23 +0900

    feat : 서울에서 김서방 찾기

---
+++ Level1/서울에서_김서방_찾기.java
@@ -0,0 +1,21 @@
+package Level1;
+
+public class 서울에서_김서방_찾기 {
+
+    public static String solution(String[] seoul) {
+
+        int key = 0;
+        for (int x = 0; x < seoul.length; x++) {
+            if (seoul[x].equals("Kim")) {
+                key = x;
+                break;
+            }
+        }
+        return "김서방은 " + key + "에 있다";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(new String[]{"jane", "kim", "Kim"}));
+    }
+}
