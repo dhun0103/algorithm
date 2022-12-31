3b0d473f1d0bc029331367878ad70151f14cf821
Jeongdonghun <arrrrr180@gmail.com>
2022-12-31 17:02:52 +0900

    feat : 수박수박수박수박수박수

---
+++ Level1/수박수박수박수박수박수.java
@@ -0,0 +1,20 @@
+package Level1;
+
+public class 수박수박수박수박수박수 {
+
+    public static String solution(int n) {
+
+        StringBuilder str = new StringBuilder();
+        for (int x = 0; x < n; x++) {
+            if (x % 2 == 0) str.append("수");
+            else str.append("박");
+        }
+
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(4));
+    }
+}
