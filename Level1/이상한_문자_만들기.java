a509af1b9c55642570195fceb022ed28837e16fa
Jeongdonghun <arrrrr180@gmail.com>
2023-01-12 23:32:24 +0900

    feat : 이상한 문자 만들기

---
+++ Level1/이상한_문자_만들기.java
@@ -0,0 +1,27 @@
+package Level1;
+
+public class 이상한_문자_만들기 {
+
+    public static String Solution(String s) {
+
+        String[] str = s.split("");
+        String answer = "";
+        int cnt = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals("")) {
+                answer += str[i];
+                cnt = 0;
+            } else {
+                answer += (cnt % 2 == 0) ? str[i].toUpperCase() : str[i].toLowerCase();
+                cnt++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Solution("try hello world"));
+    }
+}
