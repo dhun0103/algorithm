bc5ffc754b127a2cbc08517aa752c43c3c9bb077
Jeongdonghun <arrrrr180@gmail.com>
2023-01-16 23:49:16 +0900

    feat : 자연수 뒤집어 배열로 만들기

---
+++ Level1/자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,24 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class 자연수_뒤집어_배열로_만들기 {
+
+    public static int[] solution(long n) {
+
+        String s = Long.toString(n);
+        int[] arr = new int[s.length()];
+
+        int cnt = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            arr[cnt++] = Integer.parseInt(s.substring(i, i + 1));
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(solution(12345)));
+    }
+}
